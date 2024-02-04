8e4fcfc5f2f4deaa13edbd7363e8339bf3341bab
ozatamago <ozatamago40@gmail.com>
2024-02-05 00:26:52 +0900

    caluculate_targetの追加

--- trainer/policy_trainer.py
+++ trainer/policy_trainer.py
@@ -1,6 +1,7 @@
 import torch
 import torch.optim as optim
 from torch.distributions import Categorical
+import torch.nn.functional as F
 
 class PolicyNetworkTrainer:
     def __init__(self, policy_network, critic_network, q_network, learning_rate=1e-3, gamma=0.99, lambda_gae=0.95):
@@ -11,15 +12,23 @@ class PolicyNetworkTrainer:
         self.gamma = gamma
         self.lambda_gae = lambda_gae
 
-    @staticmethod
-    def calculate_advantage(rewards, states, next_states, dones, critic_network, q_network, gamma=0.99, lambda_gae=0.95):
+    def calculate_target(self, rewards, next_states, dones, critic_network, gamma=0.99, lambda_gae=0.95):
+        y_lambda = torch.zeros_like(rewards)
+        next_value = critic_network(next_states[-1]).detach()
+        for t in reversed(range(len(rewards))):
+            next_value = rewards[t] + gamma * next_value * (1 - dones[t])
+            y_lambda[t] = next_value
+            for n in range(1, len(rewards) - t):
+                G_tn = sum([gamma ** (l - 1) * rewards[t + l - 1] for l in range(1, n + 1)])
+                if t + n < len(rewards):
+                    G_tn += gamma ** n * critic_network(next_states[t + n]).detach()
+                y_lambda[t] += (1 - lambda_gae) * lambda_gae ** (n - 1) * G_tn
+        return y_lambda
+
+    # @staticmethod
+    def calculate_advantage(self, rewards, states, next_states, dones, critic_network, q_network, gamma=0.99, lambda_gae=0.95):
         # TD(λ)によるy(λ)の計算
-        y_lambda = 0
-        for n in range(1, len(rewards) + 1):
-            G_tn = sum([gamma ** (l - 1) * rewards[l - 1] for l in range(1, n + 1)])
-            if n < len(rewards):
-                G_tn += gamma ** n * critic_network(next_states[n]).item()
-            y_lambda += (1 - lambda_gae) * lambda_gae ** (n - 1) * G_tn
+        y_lambda = self.calculate_target(rewards, next_states, dones, gamma)
 
         # 状態行動価値関数 Qψ の計算
         Q_values = q_network(states)
@@ -40,3 +49,54 @@ class PolicyNetworkTrainer:
         self.optimizer.step()
 
         return policy_loss.item()
+    
+
+# 学習ループの例
+# # 方策ネットワークとクリティックネットワーク、およびQネットワークのインスタンス化
+# policy_network = ...  # PolicyNetworkのインスタンス
+# critic_network = ...  # CriticNetworkのインスタンス
+# q_network = ...  # QNetworkのインスタンス
+
+# # 学習用トレーナーのインスタンス化
+# trainer = PolicyNetworkTrainer(policy_network, critic_network, q_network)
+
+# # 学習パラメータ
+# num_episodes = 1000  # 総エピソード数
+# max_steps = 100  # 各エピソードの最大ステップ数
+
+# for episode in range(num_episodes):
+#     states = []  # 状態を保存するリスト
+#     actions = []  # 行動を保存するリスト
+#     rewards = []  # 報酬を保存するリスト
+#     next_states = []  # 次の状態を保存するリスト
+#     dones = []  # エピソード終了フラグを保存するリスト
+
+#     # 環境の初期化
+#     state = env.reset()
+#     for step in range(max_steps):
+#         # 方策ネットワークから行動を選択
+#         action = policy_network.select_action(state)
+        
+#         # 選択した行動を環境に適用
+#         next_state, reward, done, _ = env.step(action)
+        
+#         # データの保存
+#         states.append(state)
+#         actions.append(action)
+#         rewards.append(reward)
+#         next_states.append(next_state)
+#         dones.append(done)
+
+#         # 次の状態への更新
+#         state = next_state
+
+#         if done:
+#             break
+
+#     # 1エピソード分の学習
+#     policy_loss = trainer.train_step(states, actions, rewards, next_states, dones)
+#     print(f"Episode {episode}, Loss: {policy_loss}")
+
+#     # エピソード終了処理（任意で追加可能）
+#     ...
+
